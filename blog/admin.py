c589de938914d759967abdd24412115aa39f25a8
ParseSite <53075834+ParseSite@users.noreply.github.com>
2019-07-23 19:52:12 +0430

    Like comment added

--- blog/admin.py
+++ blog/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Category, Post, Comment, Location, SavePost
+from .models import Category, Post, Comment, Location, SavePost, LikeComment
 
 admin.site.register(Category)
 admin.site.register(Post)
 admin.site.register(Comment)
 admin.site.register(Location)
 admin.site.register(SavePost)
+admin.site.register(LikeComment)
